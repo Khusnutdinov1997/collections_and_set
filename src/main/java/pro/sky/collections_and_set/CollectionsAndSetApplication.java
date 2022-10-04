04fb903e3a4db9189a12df7435107ac3d7397264
Emil <emiles1997@gmail.com>
2022-10-04 20:35:19 +0300

    первый коммит

---
+++ src/main/java/pro/sky/collections_and_set/CollectionsAndSetApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.collections_and_set;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionsAndSetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionsAndSetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/collections_and_set/CollectionsAndSetApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.collections_and_set;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionsAndSetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
